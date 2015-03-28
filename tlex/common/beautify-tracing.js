71b27dad70dd985891c39b7ed77e421d88fcb9a8
Prev (Young Soo Lee) <prevdev@gmail.com>
2015-03-28 19:13:20 +0900

    improve beautify-tracing

--- tlex/common/beautify-tracing.js
+++ tlex/common/beautify-tracing.js
@@ -3,13 +3,29 @@
 		var traces = document.getElementsByClassName("tlex-var-trace");
 		for (var i=0; i<traces.length; i++) {
 			var str = traces[i].innerHTML;
-			str = str.replace(/( *)\[(.*)\]=/g, '$1[<b>$2</b>]=');
-			str = str.replace(/( *)(array|object)(.*?){\n/g, '$1<span class="vtrace-keyword">$2</span>$3<b>{</b>\n');
-			str = str.replace(/( *)string\(([0-9]*)\)\s"([\S\s]*?)"\n/g, '$1<span class="vtrace-keyword">string</span>($2) <span class="vtrace-string">"$3"</span>\n');
-			str = str.replace(/( *)int\(([0-9]*)\)\n/g, '$1<span class="vtrace-keyword">int</span>(<span class="vtrace-light">$2</span>)\n');
-			str = str.replace(/( *)bool\((true|false)\)\n/g, '$1<span class="vtrace-keyword">bool</span>(<span class="vtrace-light"><b>$2</b></span>)\n');
-			str = str.replace(/( *)NULL\n/g, '$1<span class="vtrace-light"><i>NULL</i></span>\n');
-			str = str.replace(/( *)}\n/g, '$1<b>}</b>\n');
+			
+			str = str.replace(/( *)\[(.*)\]=(?:>|&gt;)\n(\s*)/g, function (match, p1, p2, p3) {
+				p1 = p1.split("  ").join('<span class="tlex-var-trace-space"><i></i></span>');
+				return p1 + '[<span class="tlex-var-trace-keyword">' + p2 + '</span>] <span class="sub">=></span> ';
+			});
+			
+			str = str.replace(/( *)(array|object)(.*?){\n/g, '$1<span class="tlex-var-trace-vartype2">$2</span>$3<span class="tlex-var-trace-vartype2">{</span>\n');
+			str = str.replace(/( *)string\(([0-9]*)\)\s"([\S\s]*?)"\n/g, function (match, p1, p2, p3) {
+				p3 = p3.replace(/&/g, "&amp;")
+				.replace(/</g, "&lt;")
+				.replace(/>/g, "&gt;");
+
+				return p1 + '<span class="tlex-var-trace-vartype">string</span>('+p2+') <b>"</b><span class="tlex-var-trace-string">'+p3+'</span><b>"</b>\n';
+			});
+			str = str.replace(/( *)int\((\-?[0-9]*)\)\n/g, '$1<span class="tlex-var-trace-vartype3">int</span>(<span class="tlex-var-trace-numric">$2</span>)\n');
+			str = str.replace(/( *)bool\((true|false)\)\n/g, '$1<span class="tlex-var-trace-vartype3">bool</span>(<span class="tlex-var-trace-bool">$2</span>)\n');
+			str = str.replace(/( *)NULL\n/g, '$1<span class="tlex-var-trace-bool"><i>NULL</i></span>\n');
+			
+			//str = str.replace(/( *)}\n/g, '$1<span class="tlex-var-trace-vartype2">}</span>\n');
+			str = str.replace(/( *)}(\n?)/g, function (match, p1, p2) {
+				p1 = p1.split("  ").join('<span class="tlex-var-trace-space"><i></i></span>');
+				return p1 + '<span class="tlex-var-trace-vartype2">}</span>' + p2;
+			});
 			
 			traces[i].innerHTML = str;
 		}
